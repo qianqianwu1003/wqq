b86dee635bc1f94c3b8b2914792a0b587ee56161
wuqq <wuqq>
2020-10-14 22:49:55 +0800

    wqq

---
+++ AudioWmp/pages/mystory/index/index.js
@@ -0,0 +1,113 @@
+// pages/mystory/index/index.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    //轮播图集合
+    banner_images: [{
+      img:'/pages/images/lbt.jpg',
+      SaveText:'123',
+      id:1,
+      urlPath:''
+    }],
+    //菜单导航集合
+    menu_channelList:[{
+      ChannelID:1,
+      ChannelName:'早安父母',
+      ChannelLogo:'/pages/images/home_community.png'
+    },{
+      ChannelID:1,
+      ChannelName:'课程',
+      ChannelLogo:'/pages/images/doorkey.png'
+    },{
+      ChannelID:1,
+      ChannelName:'好书解读',
+      ChannelLogo:'/pages/images/home_community.png'
+    },{
+      ChannelID:1,
+      ChannelName:'免费专区',
+      ChannelLogo:'/pages/images/home_community.png'
+    },{
+      ChannelID:1,
+      ChannelName:'共读营',
+      ChannelLogo:'/pages/images/home_community.png'
+    },{
+      ChannelID:1,
+      ChannelName:'终身成长',
+      ChannelLogo:'/pages/images/home_community.png'
+    },{
+      ChannelID:1,
+      ChannelName:'孩子听',
+      ChannelLogo:'/pages/images/home_community.png'
+    },{
+      ChannelID:1,
+      ChannelName:'名师讲座',
+      ChannelLogo:'/pages/images/home_community.png'
+    },{
+      ChannelID:1,
+      ChannelName:'不吼不叫',
+      ChannelLogo:'/pages/images/home_community.png'
+    },{
+      ChannelID:1,
+      ChannelName:'热榜',
+      ChannelLogo:'/pages/images/home_community.png'
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
